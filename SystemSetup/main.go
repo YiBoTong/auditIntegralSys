68a0c9f304a37878cbd17720366a145964e49c38
e <434848237@qq.com>
2018-11-23 15:28:44 +0800

    add status 500 handler

--- SystemSetup/main.go
+++ SystemSetup/main.go
@@ -6,6 +6,7 @@ import (
 	"auditIntegralSys/_public/log"
 	"auditIntegralSys/_public/router"
 	"gitee.com/johng/gf/g"
+	"gitee.com/johng/gf/g/net/ghttp"
 )
 
 const (
@@ -20,6 +21,9 @@ func main() {
 	s.BindController(apiPath+"/login", new(handler.Login))
 	s.BindController(apiPath+"/log", new(handler.Log))
 	s.BindHandler("/*", router.Index)
+	s.BindStatusHandlerByMap(map[int]ghttp.HandlerFunc{
+		500: router.Status_500,
+	})
 	s.SetLogPath(config.LogPath)
 	s.SetAccessLogEnabled(true)
 	s.SetErrorLogEnabled(true)
