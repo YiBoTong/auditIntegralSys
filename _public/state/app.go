18468bf47f6935d3af72784522a1f8b927e8fcf5
yc <434848237@qq.com>
2018-12-28 17:10:44 +0800

    公共状态：优化

---
+++ _public/state/app.go
@@ -0,0 +1,9 @@
+package state
+
+const (
+	Draft   string = "draft"   // 草稿
+	Report  string = "report"  // 上报
+	Adopt   string = "adopt"   // 通过
+	Reject  string = "reject"  // 驳回
+	Publish string = "publish" // 发布
+)
---
+++ docker/removeOldData/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "gitee.com/johng/gf/g"
+
+func main()  {
+	db:=g.DB()
+
+}
